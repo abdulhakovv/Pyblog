87cbce603cd9d86ff25f7c3f607e41e9bb3ab49d
abdulhakovv <abdulhaqoffabdulhodiy@gmail.com>
2023-05-08 23:27:17 +0500

    insho

--- js/main.js
+++ js/main.js
@@ -2,11 +2,21 @@ let elmoon_dark_light_btn = document.querySelector(".header-inner__services-moon
 let elsun_dark_light_btn = document.querySelector(".header-inner__services-sun")
 let elInput_search = document.querySelector(".services__search-input")
 let elhistory_list = document.querySelector(".services__history-list")
+let elEssay_link = document.querySelector(".services__essay-link")
+let elEssay_saving = document.querySelector(".services__essay-saving")
+let elXmark = document.querySelector(".fa-xmark")
 
 window.addEventListener("click", (evt)=>{
   if(!evt.target.matches(".services__search-input")){
     elhistory_list.classList.remove("active");
   }
+})
+
+elEssay_link.addEventListener("click", ()=>{
+  elEssay_saving.classList.add("show-save")
+})
+elXmark.addEventListener("click", ()=>{
+  elEssay_saving.classList.remove("show-save")
 })
 
 elmoon_dark_light_btn.addEventListener("click", ()=>{
